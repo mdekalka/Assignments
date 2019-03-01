1bd7a46adb4a0de42e8c346f1bd724c744a89017
Aleh Isakau <aleh.isakau@itechart-group.com>
2019-03-01 13:02:17 +0300

    added login form styles

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import Paper from '@material-ui/core/Paper';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Lock from '@material-ui/icons/Lock';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import logo from '../assets/icons/logo.svg'
+
 
 const styles = (theme) => ({
   root: {
@@ -13,14 +18,26 @@ const styles = (theme) => ({
     justifyContent: 'center',
     alignItems:'center',
   },
-
   paper: {
-    padding: theme.spacing.unit * 2,
+    padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 3}px`,
   },
-
+  logo: {
+    marginRight: theme.spacing.unit * 2
+  },
+  headline: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing.unit * 3
+  },
+  formControl: {
+    marginBottom: theme.spacing.unit * 2
+  },
   form: {
-    maxWidth: 450,
-    backgroundColor: theme.palette.background.default
+    maxWidth: 350,
+    // backgroundColor: theme.palette.background.default
+  },
+  button: {
+    marginTop: theme.spacing.unit * 2
   }
 })
 
@@ -28,7 +45,8 @@ class LoginPage extends Component {
   state = {
     username: '',
     password: '',
-    error: null
+    error: null,
+    submitted: false
   }
 
   onInputChange = (event) => {
@@ -39,46 +57,55 @@ class LoginPage extends Component {
     })
   }
 
+  onSignIn = (event) => {
+    event.preventDefault()
+
+    this.setState({ submitted: true })
+  }
+
+  isInvalidField(field) {
+    return this.state.submitted && !field.trim().length
+  }
+
   render() {
-    const { username, password, error,  } = this.state
+    const { username, password, error } = this.state
     const { classes } = this.props
 
     return (
       <div className={classes.root}>
-        <Paper className={classes.paper} elevation={4}>
-          <form noValidate autoComplete="off" className={classes.form}>
-            <TextField
-              label="Name"
-              name="username"
-              // className={classes.textField}
-              value={username}
-              onChange={this.onInputChange}
-              margin="normal"
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              label="Password"
-              name="password"
-              // className={classes.textField}
-              value={password}
-              onChange={this.onInputChange}
-              margin="normal"
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Lock />
-                  </InputAdornment>
-                ),
-              }}
-            />
+        <Paper className={classes.paper} elevation={16}>
+          <div className={classes.headline}>
+            <img src={logo} alt="main logo" className={classes.logo} />
+            <Typography variant="h4">Assignments</Typography>
+          </div>
+
+          <Typography variant="subtitle1">Login to your account:</Typography>
+
+          <form noValidate autoComplete="off" className={classes.form} onSubmit={this.onSignIn}>
+
+            <FormControl fullWidth error={this.isInvalidField(username)} className={classes.formControl}>
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                name="username"
+                value={username}
+                onChange={this.onInputChange}
+              />
+              { this.isInvalidField(username) && <FormHelperText>Username is required</FormHelperText> }
+            </FormControl>
+
+            <FormControl fullWidth error={this.isInvalidField(password)} className={classes.formControl}>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                value={password}
+                onChange={this.onInputChange}
+              />
+              { this.isInvalidField(password) && <FormHelperText>Password is required</FormHelperText> }
+            </FormControl>
+
+            <Button variant="contained" color="primary" type="submit" className={classes.button} fullWidth>
+              Sign In
+            </Button>
           </form>
         </Paper>
       </div>
