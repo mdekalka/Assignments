cf6a1c69521d36027af8ff3bca8108eb98723d2f
Aleh Isakau <aleh.isakau@itechart-group.com>
2019-03-20 13:20:42 +0300

    fix prod static

--- server/app.js
+++ server/app.js
@@ -20,17 +20,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
 //build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// })
 
 app.use('/', index);
 app.use('/api/todo', todoRoutes)
