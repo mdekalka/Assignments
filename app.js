f0bdc5dde9c22a9020e73eab0d3b6ec93b252b3f
Aleh Isakau <aleh.isakau@itechart-group.com>
2019-03-20 14:02:33 +0300

    omg pls heroku fix subfolders

--- app.js
+++ app.js
@@ -28,16 +28,12 @@ app.use(function(req, res, next) {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
-//build mode
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// })
 
 app.use('/', index);
 app.use('/api/todo', todoRoutes)
